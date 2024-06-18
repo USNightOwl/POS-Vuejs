20df0399c978f1f4ec19635ff24c04bc5e1d3d47
21120447 <haonhat2729@gmail.com>
2024-06-18 15:27:15 +0700

    chore: use debounce

---
+++ learn/src/App.vue
@@ -0,0 +1,29 @@
+<script>
+import { debounce } from 'lodash-es'
+
+export default {
+  data() {
+    return {
+      count: 0,
+    }
+  },
+  methods: {
+    increment: debounce(function () {
+      this.count++
+    }, 500)
+  },
+  mounted() {
+    this.increment()
+  }
+}
+</script>
+
+<template>
+  <div class="flex flex-col items-center pt-3">
+    <div class="text-lg">{{ count }}</div>
+    <button @click="increment" type="button" class="border border-gray-900 rounded mt-1 p-1">Increment</button>
+  </div>
+</template>
+
+<style scoped>
+</style>
